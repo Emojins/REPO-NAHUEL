62ac22b5c4abd9be48b8fdb682c410ae4b7a5849
Emojins <nahu.1999@outlook.com>
2022-09-19 17:55:02 -0300

    conexion a la DB de Mongo

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const express = require('express')
 const app = express()
 const path= require("path")
-
 app.use(express.json())
+
+
+const dbConnect = require('./src/db/connection');
+
 app.use(require("./src/routes/routes"))
 app.use(express.static(path.join(__dirname, 'src/public/img')));
+
+
+dbConnect();
 
 
 app.listen(4000, ()=> {console.log("Servidor corriendo en puerto 4000")})
